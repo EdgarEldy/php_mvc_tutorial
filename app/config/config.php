3951e6bcffdb5b8eac8f696d96bdfd5dae86ab94
EdgarEldy <ed.eldy21@live.com>
2019-07-01 22:32:16 +0200

    updated config file

--- app/config/config.php
+++ app/config/config.php
@@ -1,6 +1,5 @@
 <?php
 define('DS', DIRECTORY_SEPARATOR);
-define('ROOT', dirname(dirname(__DIR__)));
 define('APP_ROOT', dirname(dirname(__FILE__)));
 define('URL_ROOT', 'http://localhost/php-mvc-tutoriel');
 define('CONTROLLERS', APP_ROOT . DS . 'controllers' . DS);
