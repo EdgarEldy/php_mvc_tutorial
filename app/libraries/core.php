be756b67fde6f428a2118a8343b051eabb497056
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-02-03 21:03:02 +0200

    Updated core.php

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -1,38 +1,33 @@
 <?php
 
 /** 
- * @author Edgard
- * permet d'instancier les controller et de recuperer les liens des views dans le naviguateur
+ * @author EdgarEldy
  */
-namespace Gestion_shift\app\libraries\core;
+namespace php_mvc_tutorial\app\libraries\core;
 
 class core
 {
 
     /**
      */
-    protected $controller='dashboard';
+    protected $controller='home';
     protected $method='index';
     protected $params=[];
     
     public function __construct()
     {
         $url=$this->getUrl();
-        
-        // On recherche dans les controllers la premiere valeur
+
         if (file_exists(CONTROLLERS . $url[0] . '.php')) {
             $this->controller=$url[0];
             unset($url[0]);
         }
-        
-        // Appel du controlleur
+
         
         require_once CONTROLLERS . $this->controller . '.php';
-        
-        // On instancie le controlleur
+
         $this->controller=new $this->controller;
-        
-        // On verifie la deuxieme valeur du lien 
+
         
         if (isset($url[1])) {
             if (method_exists($this->controller, $url[1])) {
@@ -40,8 +35,6 @@ class core
               unset($url[1]);
             }
         }
-        
-        // On recupere les parametres
         
         $this->params = $url ? array_values($url) : [] ;
         
