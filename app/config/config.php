ece82b57c89baded90605d8e005fd9e49e445e68
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-02-03 20:14:24 +0200

    Revert "Updated the config file"
    This reverts commit 5275ca10bc9d7edf412ba1ed6d762939630455be.

--- app/config/config.php
+++ app/config/config.php
@@ -1,12 +1,13 @@
 <?php
 define('DS', DIRECTORY_SEPARATOR);
-define('SITE_NAME', 'PHP-MVC-TUTORIAL');
+define('SITE_NAME', 'RENT-A-CAR APP');
 define('APP_ROOT', dirname(dirname(__FILE__)));
-define('URL_ROOT', 'http://localhost/php_mvc_tutorial');
+define('URL_ROOT', 'http://localhost/Gestion_shift');
 define('CONTROLLERS', APP_ROOT . DS . 'controllers' . DS);
 define('HELPERS', APP_ROOT . DS . 'helpers' . DS);
 define('LIB', APP_ROOT . DS . 'libraries' . DS);
 define('MODELS', APP_ROOT . DS . 'models' . DS);
 define('VIEWS', APP_ROOT . DS . 'views' . DS);
 define('INC', APP_ROOT . DS . 'views' . DS .'inc' . DS);
-define('DEFAULT', APP_ROOT . DS . 'views' . DS .'templates' . DS . 'default' . DS);
+define('DEFAULT_LAYOUT', APP_ROOT . DS . 'views' . DS .'templates' . DS . 'default_layout' . DS);
+define('NEW_LAYOUT', APP_ROOT . DS . 'views' . DS .'templates' . DS . 'new' . DS);
