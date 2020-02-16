0c30ff1397ea5094955b46b1cc5c066f163255b9
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-02-16 22:39:44 +0200

    added an sql file for foreign keys queries

--- simple_store_queries.sql
+++ simple_store_queries.sql
@@ -1,19 +1,19 @@
-alter table tbl_order add constraint fk_customer_id
-foreign key(fk_customer_id) references tbl_customer(customer_id);
+alter table `order` add constraint fk_customer_id
+foreign key(fk_customer_id) references customer(customer_id);
 
-alter table tbl_order drop foreign key fk_customer_id;
+alter table `order` drop foreign key fk_customer_id;
 
-alter table tbl_order add constraint fk_product_id
-foreign key(fk_product_id) references tbl_product(product_id);
+alter table `order` add constraint fk_product_id
+foreign key(fk_product_id) references product(product_id);
 
-alter table tbl_order drop foreign key fk_product_id;
+alter table `order` drop foreign key fk_product_id;
 
-alter table tbl_product add constraint fk_cat_id
-foreign key(fk_cat_id) references tbl_category(cat_id);
+alter table product add constraint fk_cat_id
+foreign key(fk_cat_id) references category(cat_id);
 
-alter table tbl_product drop foreign key fk_cat_id;
+alter table product drop foreign key fk_cat_id;
 
-alter table tbl_user add constraint fk_profile_id
-foreign key(fk_profile_id) references tbl_profile(profile_id);
+alter table user add constraint fk_profile_id
+foreign key(fk_profile_id) references profile(profile_id);
 
-alter table tbl_user drop foreign key fk_profile_id;
+alter table user drop foreign key fk_profile_id;
